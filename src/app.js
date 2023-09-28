25c9d3bcaeaa07aeb4264310f44e6d8e6719108d
fuseeiam <nattaporn.orr@gmail.com>
2023-09-28 11:10:50 +0700

    firstinit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || '5000';
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
