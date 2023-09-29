15a81e2f89c44f301257ba6dfc7d380c5626f6ea
fuseeiam <nattaporn.orr@gmail.com>
2023-09-29 10:11:07 +0700

    login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { registerSchema } = require('../validators/auth-validator');
+const { registerSchema, loginSchema } = require('../validators/auth-validator');
 const prisma = require('../models/prisma');
+const createError = require("../utils/create-error");
 
 exports.register = async (req, res, next) => {
     try {
@@ -36,9 +37,35 @@ exports.register = async (req, res, next) => {
 };
 exports.login = async (req, res, next) => {
     try {
-        // const {value , error} = loginSchema.validate(req.body);
-        //   if (error){}
+        const { value, error } = loginSchema.validate(req.body);
+        if (error) {
+            // error.statusCode = 400
+            return next(error);
+        }
+        //select * from user where email = emailOrMobile OR M=mobile = emailOrMobile
+        //findFirst return Obj
+
+        const user = await prisma.user.findFirst({
+            where: {
+                OR: [{ email: value.emailOrMobile }, { mobile: value.emailOrMobile }]
+            }
+        });
+        if (!user) {
+            //create function to throw errorMiddleware instread of res.status(400).json({msg:'invalid'})
+            return next(createError('invalid credential', 400));
+            //next(error)
+        }
+        const isMatch = await bcrypt.compare(value.password, user.password);
+        if (!isMatch) {
+            return next(createError('invalid credential', 400));
+        }
+
+        const payload = { userId: user.id };
+        const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY || 'jhjfdgiwgcbssjgqaz', {
+            expiresIn: process.env.JWT_EXPIRE
+        });
+        res.status(200).json({ accessToken });
     } catch (error) {
-        next(err);
+        next(error);
     }
 };
