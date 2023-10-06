d44d5b5c3fc7d5241aeba38f0f57b4406f2f76bb
fuseeiam <nattaporn.orr@gmail.com>
2023-10-06 17:09:40 +0700

    postLike

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -21,3 +21,6 @@ const storage = multer.diskStorage(
 
 const upload = multer({ storage: storage });
 module.exports = upload;
+
+//multer.memoryStorage(); 
+
---
+++ src/routes/post-route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const authenticateMiddleware = require('../middlewares/authenticated');
+const uploadMiddleware = require('../middlewares/upload');
+const postController = require('../controllers/post-controller');
+const likeController = require('../controllers/like-controller');
+
+const router = express.Router();
+
+router.post('/',
+    authenticateMiddleware,
+    uploadMiddleware.single('image'),
+    postController.createPost
+);
+
+router.get(
+    '/friend',
+    authenticateMiddleware,
+    postController.getAllPostIncludeFriendPost
+);
+
+router.post('/:postId/like', authenticateMiddleware, likeController.togglelike);
+module.exports = router;
